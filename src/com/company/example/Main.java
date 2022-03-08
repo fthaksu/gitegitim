90fbda1cc553f4315382fb48968e21fcb7896848
fthaksu <fatihaksu42@gmail.com>
2022-03-08 20:58:38 +0300

    ikinci commit
    İkinci commit yapıldı

--- src/com/company/example/Main.java
+++ src/com/company/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("first version");
+
+	    Print print = new Print();
+	    print.printSomething("second version");
     }
 
 }
--- src/com/company/example/Print.java
+++ src/com/company/example/Print.java
@@ -2,6 +2,7 @@ package com.company.example;
 
 public class Print {
     public void printSomething(String text){
+
         System.out.println((text));
     }
 }
